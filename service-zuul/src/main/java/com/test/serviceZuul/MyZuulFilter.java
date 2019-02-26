3e5f832818932ad14ff11b2f8d486daad7fb735a
yxmy <577518220@qq.com>
2019-02-26 17:35:25 +0800

    2.0

---
+++ service-zuul/src/main/java/com/test/serviceZuul/MyZuulFilter.java
@@ -0,0 +1,52 @@
+package com.test.serviceZuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class MyZuulFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext rct = RequestContext.getCurrentContext();
+        HttpServletRequest request = rct.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null){
+            log.warn("token is empty");
+            rct.setSendZuulResponse(false);
+            rct.setResponseStatusCode(401);
+            try {
+                rct.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
